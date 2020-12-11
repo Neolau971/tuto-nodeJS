9ea630804ac65c14faafa6d78347c8a7bb137c33
Neolau <ulrick.neola@hotmail.fr>
2020-12-11 15:48:56 +0100

    #0001

--- app.js
+++ app.js
@@ -15,20 +15,32 @@ app.post('/tuto/test', (req, res, next) => {
     delete req.body._id;
     const elementTest = new ElementTest({...req.body});
     elementTest.save()
-        .then(() => res.status(201).json({ message: 'objet crée'}))
-        .catch(error => res.status(400).json({ error}))
+      .then(() => res.status(201).json({ message: 'objet crée'}))
+      .catch(error => res.status(400).json({ error}))
 });
 
 app.get('/tuto/test/:id', (req, res, next) => {
     ElementTest.findOne({ _id: req.params.id })
       .then(elementTest => res.status(200).json(elementTest))
       .catch(error => res.status(404).json({ error }));
-  });
+});
+
+app.put('/tuto/test/:id', (req, res, next) => {
+    ElementTest.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/tuto/test/:id', (req, res, next) => {
+    ElementTest.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+});
 
 app.use('/tuto/test', (req, res, next) => {
     ElementTest.find()
       .then(elementTest => res.status(200).json(elementTest))
       .catch(error => res.status(400).json({ error }));
-  });
+});
 
 module.exports = app;
